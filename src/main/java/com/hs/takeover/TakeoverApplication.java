a9864b4b17d9df2465fb61db215f53e8b4dacdf4
bumjin <bumjin@gmail.com>
2024-07-21 14:28:14 +0900

    #10 Request 로깅 인터셉터 생성
     Interceptor를 Bean으로 생성
     인터셉터 Config로 등록
     request의 Method와 Paramter를 출력
     파일로 출력

--- src/main/java/com/hs/takeover/TakeoverApplication.java
+++ src/main/java/com/hs/takeover/TakeoverApplication.java
@@ -3,7 +3,6 @@ package com.hs.takeover;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 public class TakeoverApplication extends SpringBootServletInitializer {
---
+++ src/main/java/com/hs/takeover/config/WebMvcConfig.java
@@ -0,0 +1,38 @@
+package com.hs.takeover.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import com.hs.takeover.interceptor.LoggingInterceptor;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+/*
+  Java configuration file that is used for Spring MVC and Thymeleaf
+  configurations
+ */
+public class WebMvcConfig implements WebMvcConfigurer,  ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+
+
+}
